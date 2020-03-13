56dab1a840a1f6fb9cea4a0442c4bb9b38dfdb8a
wuwaaa <wuwaaa@users.noreply.github.com>
2020-03-13 18:12:12 +0800

    完善页面内容

--- pp2/app.py
+++ pp2/app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import utils
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("main.html")
+
+@app.route('/time')
+def get_time():
+    return utils.get_time()
+
 
 @app.route('/ass')
 def hello_world1():
